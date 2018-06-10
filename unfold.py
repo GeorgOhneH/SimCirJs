db05da4137765e970a129511c94af39d1747badb
GeorgOhneH <georg.schwan@edubs.ch>
2018-06-10 12:15:40 +0200

    better unfolding

--- unfold.py
+++ unfold.py
@@ -1,5 +1,6 @@
 import json
 from copy import deepcopy
+import operator
 
 
 def get_json():
@@ -37,22 +38,55 @@ def get_next_id(m):
 def remove_in(con_remove_in, new_type):
     for con in con_remove_in:
         dev_id = con['to'].split('.')[0]
+        ok = True
+        for dev in new_type['devices']:
+            if dev['id'] == dev_id:
+                if dev['type'] not in ['Toggle', 'DC', 'PushOn', 'PushOff']:
+                    ok = False
+                break
+        if not ok:
+            continue
+
         for dev_con in new_type['connectors']:
             back_id = dev_con['from'].split('.')[0]
             if back_id == dev_id:
                 remove_in([dev_con], new_type)
+
         for x in new_type['devices']:
-            if x['id'] == dev_id:
-                rem_obj = x
+            if x['id'] == dev_id and x['type'] in ['Toggle', 'DC', 'PushOn', 'PushOff']:
+                new_type['devices'].remove(x)
                 break
         try:
-            new_type['devices'].remove(rem_obj)
-        except:
-            pass
-        try:
             new_type['connectors'].remove(con)
-        except:
+        except ValueError:
             pass
+
+def remove_out(con_remove_ou, new_type):
+    for con in con_remove_in:
+        dev_id = con['to'].split('.')[0]
+        ok = True
+        for dev in new_type['devices']:
+            if dev['id'] == dev_id:
+                # if dev['type'] not in ['Toggle', 'DC', 'PushOn', 'PushOff']:
+                ok = False
+                # print(dev['type'])
+                break
+        if not ok:
+            continue
+
+        for dev_con in new_type['connectors']:
+            back_id = dev_con['from'].split('.')[0]
+            if back_id == dev_id:
+                remove_out([dev_con], new_type)
+
+        # for x in new_type['devices']:
+        #     if x['id'] == dev_id and x['type'] in ['Toggle', 'DC', 'PushOn', 'PushOff']:
+        #         new_type['devices'].remove(x)
+        #         break
+        # try:
+        #     new_type['connectors'].remove(con)
+        # except ValueError:
+        #     pass
 
 
 def cord_sort(dict):
@@ -65,6 +99,8 @@ def get_value(name, history):
     if name in list(history.keys()):
         return history[name]
     if name not in blue_prints_names:
+        if name == 'Toggle' or name == 'DC':
+            return 0
         return 1
     blue_print = blue_prints[name]
     total = 0
@@ -84,139 +120,152 @@ for dev in data['devices']:
     t += get_value(dev['type'], history)
 
 factor = 1 / 10
-print(history)
-for blue_print in blue_prints.values():
-    try:
-        value = len(blue_print['devices'])
-    except KeyError:
-        value = 1
-    blue_print['width'] = int(blue_print['width'] * factor * value)
-    blue_print['height'] = int(blue_print['height'] * factor * value)
-    for dev in blue_print['devices']:
-        dev['x'] = int(dev['x'] * factor * value)
-        dev['y'] = int(dev['y'] * factor * value)
+sorted_history = sorted(history.items(), key=operator.itemgetter(1))
+sorted_history.append((data, -1))
+# for blue_print in blue_prints.values():
+#     try:
+#         value = len(blue_print['devices'])
+#     except KeyError:
+#         value = 1
+#     blue_print['width'] = int(blue_print['width'] * factor * value)
+#     blue_print['height'] = int(blue_print['height'] * factor * value)
+#     for dev in blue_print['devices']:
+#         dev['x'] = int(dev['x'] * factor * value)
+#         dev['y'] = int(dev['y'] * factor * value)
+#
+# value = len(data['devices'])
+# data['width'] = int(data['width'] * factor * value / 2)
+# data['height'] = int(data['height'] * factor * value / 2)
+# for dev in data['devices']:
+#     dev['x'] = int(dev['x'] * factor * value / 2)
+#     dev['y'] = int(dev['y'] * factor * value / 2)
 
-value = len(data['devices'])
-data['width'] = int(data['width'] * factor * value / 2)
-data['height'] = int(data['height'] * factor * value / 2)
-for dev in data['devices']:
-    dev['x'] = int(dev['x'] * factor * value / 2)
-    dev['y'] = int(dev['y'] * factor * value / 2)
+for name, k in sorted_history:
+    if k >= 0:
+        data = deepcopy(blue_prints[name])
+    else:
+        data = name
+    finished = False
+    while not finished:
+        for device in data['devices']:
+            finished = True
+            if device['type'] in blue_prints_names:
+                # New Type Modification
+                new_type = deepcopy(blue_prints[device['type']])
+                ins = []
+                outs = []
+                ids = {}
+                con_remove_in = []
+                con_remove_out = []
+                for new_device in new_type['devices']:
+                    # Unique Id Generation
+                    old_id = new_device['id']
+                    new_id = get_next_id(m)
+                    ids[old_id] = new_id
+                    new_device['id'] = new_id
+                    if new_device['type'] == 'In':
+                        ins.append({'id': new_id, 'x': new_device['x'], 'y':  new_device['y']})
+                    if new_device['type'] == 'Out':
+                        outs.append({'id': new_id, 'x': new_device['x'], 'y':  new_device['y']})
+                ins = sorted(ins, key=cord_sort)
+                outs = sorted(outs, key=cord_sort)
+                for new_con in new_type['connectors']:
+                    # Modify connection for new ids
+                    fro = new_con['from']
+                    con_id, index = fro.split('.')
+                    fro = ids[con_id] + '.' + index
+                    new_con['from'] = fro
+                    if ids[con_id] in [x['id'] for x in ins]:
+                        con_remove_in.append(new_con)
 
-finished = False
-while not finished:
+                    to = new_con['to']
+                    con_id, index = to.split('.')
+                    to = ids[con_id] + '.' + index
+                    new_con['to'] = to
+                    if ids[con_id] in [x['id'] for x in outs]:
+                        con_remove_out.append(new_con)
+                # Remove everything what isn't necessary
+                remove_in(con_remove_in, new_type)
+                remove_out(con_remove_out, new_type)
+                if con_remove_out:
+                    print('Warnung', con_remove_out)
+                    pass
+                # Normalize
+                min_x, min_y = float('inf'), float('inf')
+                max_x, max_y = float('-inf'), float('-inf')
+                for new_device in new_type['devices']:
+                    # Collect information
+                    if new_device['x'] > max_x:
+                        max_x = new_device['x']
+
+                    if new_device['y'] > max_y:
+                        max_y = new_device['y']
+
+                    if new_device['x'] < min_x:
+                        min_x = new_device['x']
+
+                    if new_device['y'] < min_y:
+                        min_y = new_device['y']
+                for new_device in new_type['devices']:
+                    new_device['x'] += device['x'] - min_x
+                    new_device['y'] += device['y'] - min_y
+
+                dev_width = max_x - min_x
+                dev_height = max_y - min_y
+                dev_small_height = 16 * max(len(ins), len(outs))
+                # Parent Modifications
+                data['devices'].remove(device)
+                for parent_device in data['devices']:
+                    #  device['y'] + dev_height + 65 >=
+                    if parent_device['x'] >= device['x'] and device['y'] + dev_height >= parent_device['y'] >= device['y'] + dev_small_height-1:
+                        parent_device['y'] += dev_height
+                    # device['x'] + dev_width + 65 >=
+                    if parent_device['y'] >= device['y'] and device['x'] + dev_width >= parent_device['x'] >= device['x'] + 63:
+                        parent_device['x'] += dev_width // 2
+
+                dev_id = device['id']
+                for con in data['connectors']:
+                    fro = con['from']
+                    con_id, index = fro.split('.')
+                    if con_id == dev_id:
+                        num_index = int(index[2:])
+                        fro = ins[num_index]['id'] + '.in0'
+                        con['from'] = fro
+
+                    to = con['to']
+                    con_id, index = to.split('.')
+                    if con_id == dev_id:
+                        num_index = int(index[3:])
+                        to = outs[num_index]['id'] + '.out0'
+                        con['to'] = to
+
+                for devi in new_type['devices']:
+                    if devi['type'] == 'In':
+                        devi['type'] = 'Joint'
+                    if devi['type'] == 'Out':
+                        devi['type'] = 'Joint'
+
+                data['devices'] += new_type['devices']
+                data['connectors'] += new_type['connectors']
+                finished = False
+                break
+
+    max_x, max_y = float('-inf'), float('-inf')
     for device in data['devices']:
-        finished = True
-        if device['type'] in blue_prints_names:
-            # New Type Modification
-            new_type = deepcopy(blue_prints[device['type']])
-            ins = []
-            outs = []
-            ids = {}
-            con_remove_in = []
-            con_remove_out = []
-            for new_device in new_type['devices']:
-                # Unique Id Generation
-                old_id = new_device['id']
-                new_id = get_next_id(m)
-                ids[old_id] = new_id
-                new_device['id'] = new_id
-                if new_device['type'] == 'In':
-                    ins.append({'id': new_id, 'x': new_device['x'], 'y':  new_device['y']})
-                if new_device['type'] == 'Out':
-                    outs.append({'id': new_id, 'x': new_device['x'], 'y':  new_device['y']})
-            ins = sorted(ins, key=cord_sort)
-            outs = sorted(outs, key=cord_sort)
-            for new_con in new_type['connectors']:
-                # Modify connection for new ids
-                fro = new_con['from']
-                con_id, index = fro.split('.')
-                fro = ids[con_id] + '.' + index
-                new_con['from'] = fro
-                if ids[con_id] in [x['id'] for x in ins]:
-                    con_remove_in.append(new_con)
+        if device['x'] > max_x:
+            max_x = device['x']
 
-                to = new_con['to']
-                con_id, index = to.split('.')
-                to = ids[con_id] + '.' + index
-                new_con['to'] = to
-                if ids[con_id] in [x['id'] for x in outs]:
-                    con_remove_out.append(new_con)
-            # Remove everything what isn't necessary
-            remove_in(con_remove_in, new_type)
-            if con_remove_out:
-                print('Warnung', con_remove_out)
+        if device['y'] > max_y:
+            max_y = device['y']
+    data['width'] = max_x + 150
+    data['height'] = max_y + 64
+    if k >= 0:
+        blue_prints[name] = deepcopy(data)
+    else:
+        pass
 
-            # Normalize
-            min_x, min_y = float('inf'), float('inf')
-            max_x, max_y = float('-inf'), float('-inf')
-            for new_device in new_type['devices']:
-                # Collect information
-                if new_device['x'] > max_x:
-                    max_x = new_device['x']
+total_dev = len([x for x in data['devices'] if x['type'] != 'Joint'])
 
-                if new_device['y'] > max_y:
-                    max_y = new_device['y']
-
-                if new_device['x'] < min_x:
-                    min_x = new_device['x']
-
-                if new_device['y'] < min_y:
-                    min_y = new_device['y']
-            for new_device in new_type['devices']:
-                new_device['x'] += device['x'] - min_x
-                new_device['y'] += device['y'] - min_y
-
-            dev_width = max_x - min_x
-            dev_height = max_y - min_y
-            dev_small_height = 16 * max(len(ins), len(outs))
-            # Parent Modifications
-            data['devices'].remove(device)
-            for parent_device in data['devices']:
-
-                if parent_device['x'] + 65 >= device['x'] and device['y'] + dev_height + 65 >= parent_device['y'] >= device['y'] + dev_small_height-1:
-                    parent_device['y'] += dev_height
-
-                if parent_device['y'] + 65 >= device['y'] and device['x'] + dev_width + 65 >= parent_device['x'] >= device['x'] + 63:
-                    parent_device['x'] += dev_width
-
-            dev_id = device['id']
-            for con in data['connectors']:
-                fro = con['from']
-                con_id, index = fro.split('.')
-                if con_id == dev_id:
-                    num_index = int(index[2:])
-                    fro = ins[num_index]['id'] + '.in0'
-                    con['from'] = fro
-
-                to = con['to']
-                con_id, index = to.split('.')
-                if con_id == dev_id:
-                    num_index = int(index[3:])
-                    to = outs[num_index]['id'] + '.out0'
-                    con['to'] = to
-
-            data['devices'] += new_type['devices']
-            data['connectors'] += new_type['connectors']
-            finished = False
-            break
-
-
-max_x, max_y = float('-inf'), float('-inf')
-for device in data['devices']:
-    if device['type'] == 'In':
-        device['type'] = 'Joint'
-    if device['type'] == 'Out':
-        device['type'] = 'Joint'
-
-    if device['x'] > max_x:
-        max_x = device['x']
-
-    if device['y'] > max_y:
-        max_y = device['y']
-data['width'] = max_x + 150
-data['height'] = max_y + 64
-
-print(len(data['devices']), len(data['connectors']))
+print(total_dev, len(data['connectors']))
 
 print(json.dumps(data))
